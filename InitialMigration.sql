dc1323be0c4b4bf7f99d469cccbd6c7f377c8a02
Michael Häggblad <mhd@learningwell.se>
2015-12-06 15:09:31 +0100

    deleted filter statement from part of migration script of users since it refers to its own data. This touches to the 'halloween problem' and also means that we expect data to exist in the table or that duplicates exist in the source. Neither of which should be true and if they were would point to a much more troublesome scenario

--- InitialMigration.sql
+++ InitialMigration.sql
@@ -4,5 +4,4 @@ INSERT INTO @TARGET_DATABASE.dbo.Users (Id, Username, Permissions)
 	SELECT id, CONCAT(firstname, ' ', lastname), CASE WHEN role_id = 1 THEN 96 ELSE 63 END
 	FROM @SOURCE_DATABASE.dbo.TW_employees e
 	WHERE deleted = 'N'
-		AND e.id NOT IN (SELECT Id FROM @TARGET_DATABASE.dbo.Users)
 SET IDENTITY_INSERT @TARGET_DATABASE.dbo.Users OFF;
